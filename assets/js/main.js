af8911f2235b5b44e6561ec53cb0d166c6d381aa
inlancersystem4 <inlancersystem4@gmail.com>
2024-02-19 12:28:28 +0530

    add new section

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,6 +2,10 @@ var swiper = new Swiper(".mySwiper", {
     slidesPerView: "auto",
     centeredSlides: true,
     spaceBetween: 30,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
     pagination: {
         el: ".swiper-pagination",
         clickable: true,
@@ -11,5 +15,9 @@ var swiper = new Swiper(".mySwiper", {
 
 var swiper = new Swiper("#heroBanner", {
     centeredSlides: true,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
     effect: "fade",
 });
